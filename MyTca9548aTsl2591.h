ffe1f12aeff1d8a812b41ca92401830b4ff9d190
Denis Immoos <denisimmoos@gmailcom>
2021-02-07 16:14:53 +0100

    Tca95aT renamed

---
+++ MyTca9548aTsl2591.h
@@ -0,0 +1,129 @@
+/*
+  MyHCSR04.h - Library MyHCSR04 sensor.
+  Released into the public domain.
+*/
+
+#ifndef MyTca9548aTsl2591_h
+#define MyTca9548aTsl2591_h
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+
+class MyTca9548aTsl2591
+{
+  private:
+ 	int _i2cBus;
+
+  public:
+
+  MyTca9548aTsl2591(int i2cBus){
+ 		_i2cBus = i2cBus;
+	};
+
+	Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+
+	void tcaselect(uint8_t i) {
+	  if (i > 7) return;
+	  Wire.begin();
+	  Wire.beginTransmission(0x70);
+	  Wire.write(1 << i);
+	  Wire.endTransmission();
+	}
+
+	void configureSensor(void)
+	{
+	  	  tcaselect(_i2cBus);
+
+		  Serial.print(F("i2cBus:         "));
+		  Serial.println(_i2cBus);
+
+		  if (tsl.begin())
+		  {
+ 		   Serial.println(F("Found a TSL2591 sensor ..."));
+		  }
+		  else
+		  {
+		    Serial.println(F("No sensor found ... check your wiring?"));
+		    while (1);
+		 }
+
+		// You can change the gain on the fly, to adapt to brighter/dimmer light situations
+		//tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+		tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+		//tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+
+		// Changing the integration time gives you a longer time over which to sense light
+		// longer timelines are slower, but are good in very low light situtations!
+		tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+		// tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+		//tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+		// tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+		// tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+		// tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+
+		/* Display the gain and integration time for reference sake */
+		Serial.print  (F("Gain:         "));
+		tsl2591Gain_t gain = tsl.getGain();
+		switch(gain)
+		{
+		case TSL2591_GAIN_LOW:
+		Serial.println(F("1x (Low)"));
+		break;
+		case TSL2591_GAIN_MED:
+		Serial.println(F("25x (Medium)"));
+		break;
+		case TSL2591_GAIN_HIGH:
+		Serial.println(F("428x (High)"));
+		break;
+		case TSL2591_GAIN_MAX:
+		Serial.println(F("9876x (Max)"));
+		break;
+		}
+		Serial.print  (F("Timing:       "));
+		Serial.print((tsl.getTiming() + 1) * 100, DEC);
+		Serial.println(F(" ms"));
+		Serial.println(F("------------------------------------"));
+		Serial.println(F(""));
+	}
+
+	void begin() {
+  		Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+  		configureSensor();
+	}
+
+	float getLux()
+	{
+	  tcaselect(_i2cBus);
+	  uint32_t lum = tsl.getFullLuminosity();
+	  uint16_t ir, full;
+	  ir = lum >> 16;
+	  full = lum & 0xFFFF;
+	  return tsl.calculateLux(full, ir);
+	}
+
+	int getFullSpectrum()
+	{
+	  tcaselect(_i2cBus);
+	  uint16_t full = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+	  return full;
+	}
+
+	int getVisibleSpectrum()
+	{
+	  tcaselect(_i2cBus);
+	  uint16_t visible = tsl.getLuminosity(TSL2591_VISIBLE);
+	  return visible;
+	}
+
+	int getIRSpectrum()
+	{
+	  tcaselect(_i2cBus);
+	  uint16_t ir = tsl.getLuminosity(TSL2591_INFRARED);
+	  return ir;
+	}
+
+
+};
+
+#endif
--- README.md
+++ README.md
@@ -15,9 +15,19 @@ All libraries written by myself are prefixed with **My***
 The HC-SR04 Ultrasonic Range Sensor uses non-contact ultrasound sonar to measure the distance to an object - they're great for any obstacle avoiding systems on Raspberry Pi robots or rovers! The HC-SR04 consists of two ultrasonic transmitters (basically speakers), a receiver, and a control circuit. The transmitters emit a high frequency ultrasonic sound, which bounce off any nearby solid objects, and the reciever listens for any return echo. That echo is then processed by the control circuit to calculate the time difference between the signal being transmitted and received. This time can subsequently be used, along with some clever math, to calculate the distance between the sensor and the reflecting object!
 
 
-### Usage: 
+### Usage:
 
 Have a look at [MyHCSR04.ino.example](MyHCSR04.ino.example)
 
 
+## MyTca9548aTsl2591 Combined Sensor Library
 
+- [MyTca9548aTsl2591.h](MyTca9548aTsl2591.h)
+- [MyTca9548aTsl2591.ino.example](MyTca9548aTsl2591.ino.example)
+
+### Information:
+
+
+### Usage:
+
+Have a look at [MyTca9548aTsl2591.ino.example](MyTca9548aTsl2591.ino.example)
